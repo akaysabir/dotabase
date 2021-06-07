61dff1d4d31256d7e53b25e4ac39a979f5a7f12d
Akay Sabirov <akay.sabirov@nu.edu.kz>
2021-06-07 20:30:16 +0600

    fix naming mistakes

--- Dotabase/Modules/HeroDetails/Controller/HeroDetailsViewController.swift
+++ Dotabase/Modules/HeroDetails/Controller/HeroDetailsViewController.swift
@@ -12,12 +12,12 @@ class HeroDetailsViewController: UIViewController {
     
     // MARK: - Public properties
     
-    public var hero: Hero = Hero()
-    public var lore: String?
+    var hero: Hero = Hero()
+    var lore: String?
     
     // MARK: - Private properties
     
-    private lazy var viewTitle: UILabel = {
+    private lazy var viewTitleLabel: UILabel = {
         let label = UILabel()
         label.textColor = #colorLiteral(red: 0.9333333333, green: 0.9215686275, blue: 0.8666666667, alpha: 1)
         label.font = UIFont(name: "Trajan Pro", size: 21)
@@ -26,7 +26,7 @@ class HeroDetailsViewController: UIViewController {
         return label
     }()
     
-    private lazy var avatarImage: UIImageView = {
+    private lazy var avatarImageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFill
         view.layer.masksToBounds = true
@@ -56,7 +56,7 @@ class HeroDetailsViewController: UIViewController {
         return label
     }()
     
-    private lazy var loreLabel: UITextView = {
+    private lazy var loreTextView: UITextView = {
         let label = UITextView()
         label.textColor = #colorLiteral(red: 0.9333333333, green: 0.9215686275, blue: 0.8666666667, alpha: 1)
         label.font = UIFont(name: "Arial", size: 17)
@@ -78,20 +78,20 @@ class HeroDetailsViewController: UIViewController {
     // MARK: - Setup
     
     private func setupView() {
-        navigationItem.titleView = viewTitle
+        navigationItem.titleView = viewTitleLabel
         view.backgroundColor = #colorLiteral(red: 0.1058823529, green: 0.09019607843, blue: 0.09019607843, alpha: 1)
         configureSubviews()
         configureConstraints()
     }
     
     private func configureSubviews() {
-        [avatarImage, sectionTitleLabel, loreLabel].forEach {
+        [avatarImageView, sectionTitleLabel, loreTextView].forEach {
             view.addSubview($0)
         }
     }
     
     private func configureConstraints() {
-        avatarImage.snp.makeConstraints {
+        avatarImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(20)
             $0.width.equalTo(140)
@@ -100,10 +100,10 @@ class HeroDetailsViewController: UIViewController {
         
         sectionTitleLabel.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(20)
-            $0.top.equalTo(avatarImage.snp.bottom).inset(-20)
+            $0.top.equalTo(avatarImageView.snp.bottom).inset(-20)
         }
         
-        loreLabel.snp.makeConstraints {
+        loreTextView.snp.makeConstraints {
             $0.leading.trailing.bottom.equalToSuperview().inset(10)
             $0.top.equalTo(sectionTitleLabel.snp.bottom)
         }
